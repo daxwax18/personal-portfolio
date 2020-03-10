29bb726d2738f310b587c911b8ca6cf9fd047710
Daxton Cheshire <daxwax18@gmail.com>
2020-03-10 10:32:55 -0600

    Updated Starships page with CSS changes

--- starships/main.js
+++ starships/main.js
@@ -4,19 +4,19 @@ const nav = document.querySelector('.nav')
 
 const navList = document.querySelector('.navList')
 
-function populateDOM(starships) {
+function populateNav(starships) {
     starships.forEach(starship => {
     
     let anchorWrap = document.createElement('a')
     anchorWrap.href = '#'
-    anchorWrap.addEventListener("click", (event) => {
+    anchorWrap.addEventListener('click', event => {
         console.log(event)
     }) 
     
     let listItem = document.createElement('li')
     listItem.textContent = starship.name
 
-    anchorWrap.appendChild(imageItem)
+    anchorWrap.appendChild(listItem)
     navList.appendChild(anchorWrap)
     nav.appendChild(navList)
    })
