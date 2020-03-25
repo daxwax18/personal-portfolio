57448ec4f9304edba93f3fa445bd7b228908234b
Daxton Cheshire <daxwax18@gmail.com>
2020-03-25 10:59:57 -0600

    fixed congress errors to show data

--- congress/index.js
+++ congress/index.js
@@ -3,8 +3,8 @@ import { senators } from '../data/senators.js'
 const senatorDiv = document.createElement('div')
 const main = document.querySelector('main')
 
-function populateSenatorDiv(senators) {
-    senators.forEach( senator => {
+function populateSenatorDiv() {
+    senators.forEach(senator => {
         console.log(`${senator.first_name} ${senator.last_name}`)
     })
         
