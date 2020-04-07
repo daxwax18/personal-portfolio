38f12bef3abba80c507fd432e233f8a7c2db6860
Daxton Cheshire <daxwax18@gmail.com>
2020-04-07 12:35:56 -0600

    Incomplete updates

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,5 +1,4 @@
 function getPokeData(url) {
-    let pokemonGrid = document.querySelector('.pokemonGrid')
     fetch(url).then(function (response) {
         response.json().then(function (data) {
             console.log(data)
@@ -7,9 +6,42 @@ function getPokeData(url) {
           })
       })
 }
+
+let pokemonGrid = document.querySelector('.pokemonGrid')
+
 getPokeData('https://pokeapi.co/api/v2/pokemon/1/')
 
-var card = document.querySelector('.card');
+function populatePokeCards(pokeArray) {
+    letPokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    letPokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener( 'click', () => card.pokeCard.toggle('is-flipped') 
+    
+        
+    letPokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card__face--front'
+    pokeFront.textContent = 'front'
+    letPokeBack = document.createElement('div')
+    pokeBack.className = 'card__face card__face--back'
+    pokeBack.textContent = 'back'
+
+    pokeCard.appendChild(pokeFront)
+    pokeCard.appendChild(pokeBack)
+    pokeScene.appendChild(pokeCard)
+    pokemonGrid.appendChild(pokeScene)
+
+}
+
+
+/* var card = document.querySelector('.card');
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
-}); 
+}); */
+
+{/* <div class="scene">
+    <div class="card">
+       <div class="card__face card__face--front">front</div>
+          <div class="card__face card__face--back"><div><p>Hi,I'm here on the back</p></div></div>
+          </div>
+</div> */}
