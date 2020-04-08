c3680ed3cd206c132a8c2ae8affb597444a8e012
Daxton Cheshire <daxwax18@gmail.com>
2020-04-08 10:52:01 -0600

    better way to retrieve pokedata

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,47 +1,73 @@
+/* const allPokemon = []
 function getPokeData(url) {
-    fetch(url).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data)
-            //pokemonGrid.textContent = data.name
+  fetch(url).then(function (response) {
+    response.json().then(function (pokeData) {
+      console.log(pokeData.results)
+      const pokeMap = pokeData.results.map(pokemon => {
+        return fetch(pokemon.url).then(resData => {
+          resData.json().then(pokeJson => {
+            allPokemon.push(pokeJson)
           })
+          
+        })
       })
+    })
+  })
+} */
+
+// Reusable async function to fetch data from the provided url
+async function getAPIData(url) {
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
 }
+
+// now, use the async getAPIData function
+getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then((data) => {
+  for (const pokemon of data.results) {
+    getAPIData(pokemon.url).then((pokeData) => {
+      populatePokeCards(pokeData)
+    })
+  }
+})
 
 let pokemonGrid = document.querySelector('.pokemonGrid')
 
-getPokeData('https://pokeapi.co/api/v2/pokemon/1/')
+//getPokeData('https://pokeapi.co/api/v2/pokemon?&limit=25')
 
-function populatePokeCards(pokeArray) {
-    letPokeScene = document.createElement('div')
+function populatePokeCards(singlePokemon) {
+    let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
-    letPokeCard = document.createElement('div')
+    let pokeCard = document.createElement('div')
     pokeCard.className = 'card'
-    pokeCard.addEventListener( 'click', () => card.pokeCard.toggle('is-flipped'))
-    
-        
-    letPokeFront = document.createElement('div')
+    pokeCard.addEventListener('click', () =>
+      pokeCard.classList.toggle('is-flipped'),
+    )
+    let pokeFront = document.createElement('div')
     pokeFront.className = 'card__face card__face--front'
-    pokeFront.textContent = 'front'
-    letPokeBack = document.createElement('div')
+    pokeFront.textContent = singlePokemon.name
+    let pokeBack = document.createElement('div')
     pokeBack.className = 'card__face card__face--back'
     pokeBack.textContent = 'back'
-
     pokeCard.appendChild(pokeFront)
     pokeCard.appendChild(pokeBack)
     pokeScene.appendChild(pokeCard)
     pokemonGrid.appendChild(pokeScene)
-
 }
 
-
-/* var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-}); */
-
-{/* <div class="scene">
+/* var card = document.querySelector('.card')
+card.addEventListener('click', function () {
+  card.classList.toggle('is-flipped')
+}) */
+{
+  /*   <div class="scene">
     <div class="card">
-       <div class="card__face card__face--front">front</div>
-          <div class="card__face card__face--back"><div><p>Hi,I'm here on the back</p></div></div>
-          </div>
-</div> */}
+      <div class="card__face card__face--front">front</div>
+      <div class="card__face card__face--back">back</div>
+    </div>
+  </div> */
+}
